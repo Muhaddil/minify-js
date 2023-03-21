120d047ab9f47478415fdadb894c5f54aa6fad17
Lenni <guentherhanslphdyt@gmail.com>
2023-03-21 23:29:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-npm install -g minify
-npm install -g strip-css-comments-cli
+npm install -g @prasadrajandran/strip-comments-cli
+npm install -g uglify-js
 apt-get update
 apt-get -y install moreutils
+
+minify_js(){
+    directory=$1
+    basename=$(basename $directory);
+    extension="${basename##*.}"
+    output="";
+    if [ -z "$INPUT_OUTPUT" ]
+    then
+        output="${directory%/*}/"
+    else
+        mkdir -p $INPUT_OUTPUT
+        output="$INPUT_OUTPUT";
+    fi
+    filename="${basename%.*}"
+    output_path="${output}${filename}.min.${extension}"
+    rm ${output_path}
+
+    if [ "$INPUT_OVERWRITE" = "true" ]
+    then
+      output_path="${output}${filename}.${extension}"
+    fi
+    uglifyjs --compress --mangle --output ${output_path} -- ${directory}
+    echo "Minified ${directory} > ${output_path}"
+}
 
 minify_file(){
     directory=$1
@@ -24,51 +48,31 @@ minify_file(){
     then
       output_path="${output}${filename}.${extension}"
     fi
-
-    minify ${directory} | sponge ${output_path}
+    stripcomments -w ${directory}
+    rm_whitespace(${directory})
     echo "Minified ${directory} > ${output_path}"
 }
 
-minify_css(){
+rm_whitespace() {
     directory=$1
-    basename=$(basename $directory);
-    extension="${basename##*.}"
-    output="";
-    if [ -z "$INPUT_OUTPUT" ]
-    then
-        output="${directory%/*}/"
-    else
-        mkdir -p $INPUT_OUTPUT
-        output="$INPUT_OUTPUT";
-    fi
-    filename="${basename%.*}"
-    output_path="${output}${filename}.min.${extension}"
-    rm ${output_path}
-
-    if [ "$INPUT_OVERWRITE" = "true" ]
-    then
-      output_path="${output}${filename}.${extension}"
-    fi
     sed -i -e ':a;N;$!ba;s/\n//g;s/\t//g;s/\s\{2,\}/ /g' ${directory}
-    strip-css-comments ${directory} | sponge ${output_path}
-    echo "Minified ${directory} > ${output_path}"
 }
 
 if [ -z "$INPUT_DIRECTORY" ]
 then
-    find . -type f \( -iname \*.css \) | while read fname
+    find . -type f \( -iname \*.js \) | while read fname
         do
             if [[ "$fname" != *"min."* ]]; then
-                minify_css $fname
+                minify_js $fname
             fi
         done
-    find . -type f \( -iname \*.html -o -iname \*.js \) | while read fname
+    find . -type f \( -iname \*.html -o -iname \*.css \) | while read fname
         do
             if [[ "$fname" != *"min."* ]]; then
                 minify_file $fname
             fi
         done
 else
+    minify_js $INPUT_DIRECTORY
     minify_file $INPUT_DIRECTORY
-    minify_css $INPUT_DIRECTORY
 fi
