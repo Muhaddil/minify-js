95017bd575c24a87d6b082208137455a8b27a092
Lenni <guentherhanslphdyt@gmail.com>
2023-03-21 22:19:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 npm install -g minify
+npm install -g strip-css-comments-cli
 apt-get update
 apt-get -y install moreutils
 
@@ -28,9 +29,34 @@ minify_file(){
     echo "Minified ${directory} > ${output_path}"
 }
 
+minify_css(){
+    directory=$1
+    basename=$(basename $directory);
+    extension="${basename##*.}"
+    output="";
+    if [ -z "$INPUT_OUTPUT" ]
+    then
+        output="${directory%/*}/"
+    else
+        mkdir -p $INPUT_OUTPUT
+        output="$INPUT_OUTPUT";
+    fi
+    filename="${basename%.*}"
+    output_path="${output}${filename}.min.${extension}"
+    rm ${output_path}
+
+    if [ "$INPUT_OVERWRITE" = "true" ]
+    then
+      output_path="${output}${filename}.${extension}"
+    fi
+
+    strip-css-comments ${directory} ${output_path}
+    echo "Minified ${directory} > ${output_path}"
+}
+
 if [ -z "$INPUT_DIRECTORY" ]
 then
-    find . -type f -iname \*.css -exec sed -i -e ':a;N;$!ba;s/\n//g;s/\t//g;s/\s\{2,\}/ /g;s/\/\*.*\*\///g' {} \;
+    find . -type f -iname \*.css -exec sed -i -e ':a;N;$!ba;s/\n//g;s/\t//g;s/\s\{2,\}/ /g' {} \;
     find . -type f \( -iname \*.html -o -iname \*.js \) | while read fname
         do
             if [[ "$fname" != *"min."* ]]; then
@@ -39,5 +65,5 @@ then
         done
 else
     minify_file $INPUT_DIRECTORY
-    find . -type f -iname "$INPUT_DIRECTORY/*.css" -exec sed -i -e ':a;N;$!ba;s/\n//g;s/\t//g;s/\s\{2,\}/ /g;s/\/\*.*\*\///g' {} \;
+    find . -type f -iname "$INPUT_DIRECTORY/*.css" -exec sed -i -e ':a;N;$!ba;s/\n//g;s/\t//g;s/\s\{2,\}/ /g' {} \;
 fi
