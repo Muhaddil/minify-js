c4b11f60133c3623c8814d75753d60694763f83e
Lenni <guentherhanslphdyt@gmail.com>
2023-03-22 20:28:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 npm install -g @prasadrajandran/strip-comments-cli
 npm install -g uglify-js
+npm install -g clean-css-cli
 apt-get update
 apt-get -y install moreutils
 
@@ -28,7 +29,31 @@ minify_js(){
     echo "Minified ${directory} > ${output_path}"
 }
 
-minify_file(){
+minify_css(){
+    directory=$1
+    basename=$(basename $directory);
+    extension="${basename##*.}"
+    output="";
+    if [ -z "$INPUT_OUTPUT" ]
+    then
+        output="${directory%/*}/"
+    else
+        mkdir -p $INPUT_OUTPUT
+        output="$INPUT_OUTPUT";
+    fi
+    filename="${basename%.*}"
+    output_path="${output}${filename}.min.${extension}"
+    rm ${output_path}
+
+    if [ "$INPUT_OVERWRITE" = "true" ]
+    then
+      output_path="${output}${filename}.${extension}"
+    fi
+    cleancss -o ${directory} ${directory} --inline none
+    echo "Minified ${directory} > ${output_path}"
+}
+
+minify_html(){
     directory=$1
     basename=$(basename $directory);
     extension="${basename##*.}"
@@ -61,13 +86,20 @@ then
                 minify_js $fname
             fi
         done
-    find . -type f \( -iname \*.html -o -iname \*.css \) | while read fname
+    find . -type f \( -iname \*.css \) | while read fname
         do
             if [[ "$fname" != *"min."* ]]; then
-                minify_file $fname
+                minify_css $fname
+            fi
+        done
+    find . -type f \( -iname \*.html \) | while read fname
+        do
+            if [[ "$fname" != *"min."* ]]; then
+                minify_html $fname
             fi
         done
 else
     minify_js $INPUT_DIRECTORY
-    minify_file $INPUT_DIRECTORY
+    minify_css $INPUT_DIRECTORY
+    minify_html $INPUT_DIRECTORY
 fi
