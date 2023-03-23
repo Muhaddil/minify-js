c547b79b68236dd4ffa2d0ecb8e4ecc9373f3a51
Lenni009 <guentherhanslphdyt@gmail.com>
2023-03-23 16:18:22 +0100

    fix import

--- minify_html.js
+++ minify_html.js
@@ -1,4 +1,4 @@
-import { HTMLParser } from 'npm:node-html-parser';
+import { parse } from 'npm:node-html-parser';
 
 const inputFile = Deno.args[0];
 
@@ -6,7 +6,7 @@ const inputFileContent = Deno.readTextFileSync(inputFile);
 
 const regex = /(?:\t+|\r\n|\r|\n)+/g;
 
-const dom = HTMLParser.parse(inputFileContent).removeWhitespace();
+const dom = parse(inputFileContent).removeWhitespace();
 const output = dom.toString().replace(regex, ' ');
 
 console.log(output);
