a88dfeaf2db812731a0addec8daf73dc7f00873d
Lenni009 <guentherhanslphdyt@gmail.com>
2023-03-23 17:43:57 +0100

    Revert "better whitespace stripping"
    This reverts commit fea911adfaef465c4ab079f8b8977430e5551803.

--- minify_html.js
+++ minify_html.js
@@ -4,10 +4,9 @@ const inputFile = Deno.args[0];
 
 const inputFileContent = Deno.readTextFileSync(inputFile);
 
-const regex = /(?:\t|\r\n|\r|\n)+/g;
-const whiteSpaceRegex = /(?:\s)+/g;
+const regex = /(?:\s|\r\n|\r|\n)+/g;
 
 const dom = parse(inputFileContent).removeWhitespace();
-const output = dom.toString().replace(regex, ' ').replace(whiteSpaceRegex, ' ');
+const output = dom.toString().replaceAll('\t', '').replace(regex, ' ');
 
 console.log(output);
